32ec670776f6a333896a34c5ed8957fcf85df49b
yusuferturkk <yusuf_erturk98@hotmail.com>
2023-05-09 19:26:25 +0300

    Add project files.

---
+++ Infrastructure/ECommerceApp.Persistence/Contexts/ECommerceAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ECommerceApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Persistence.Contexts
+{
+    public class ECommerceAppDbContext : DbContext
+    {
+        public ECommerceAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
